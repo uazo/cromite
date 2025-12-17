90f190c76c7c2e830926659b54a7abb276bcc31d
Carmelo Messina <uazo@users.noreply.github.com>
2025-12-17 11:32:24 +0100

    [TOOLS] Update runner to 2.329.0

--- tools/images/github-runner/start-runner.sh
+++ tools/images/github-runner/start-runner.sh
@@ -45,7 +45,7 @@ do
     -v /casefold:/win_sdk \
     --network none \
     --device=/dev/kvm \
-    uazo/github-runner:2.328.0
+    uazo/github-runner:2.329.0
 
   echo "You can stop now"
   sleep 5s
